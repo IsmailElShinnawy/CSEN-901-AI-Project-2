63c699f1e5175c79fbcb59de8c9e09688e454590
Ismail El Shinnawy <ismailelshennawy@gmail.com>
2022-12-16 23:24:06 +0200

    use reverse_action instead of action

--- CG.pl
+++ CG.pl
@@ -1,35 +1,32 @@
 :- include('KB.pl').
+% :- include('KB2.pl').
 
-action(up, Row, Col, P, Ships, Row1, Col, P, Ships) :- 
-  Row1 is Row-1,
-  Row1 >= 0.
-action(down, Row, Col, P, Ships, Row1, Col, P, Ships) :- 
-  Row1 is Row+1,
-  grid(N, _),
-  Row1 < N.
-action(left, Row, Col, P, Ships, Row, Col1, P, Ships) :- 
-  Col1 is Col-1,
-  Col1 >= 0.
-action(right, Row, Col, P, Ships, Row, Col1, P, Ships) :- 
-  Col1 is Col+1,
-  grid(_, M),
-  Col1 < M.
-action(pickup, Row, Col, P, Ships, Row, Col, P1, Ships1) :-
-  ship_at_location(Row, Col, Ships),
+reverse_action(drop, Row, Col, 0, Ships, Row, Col, 1, Ships) :-
+  station(Row, Col).
+reverse_action(drop, Row, Col, 0, Ships, Row, Col, 2, Ships) :-
   capacity(MC),
-  P1 is P+1,
-  P1 =< MC,
-  remove_ship_at_location(Row, Col, Ships, Ships1).
-action(drop, Row, Col, P, Ships, Row, Col, 0, Ships) :-
-  station(Row, Col),
-  P > 0.
+  MC = 2,
+  station(Row, Col).
+reverse_action(pickup, Row, Col, Current_Passengers, Current_Ships, Row, Col, Previous_Passengers, Previous_Ships) :-
+  \+ member([Row, Col], Current_Ships),
+  capacity(MC),
+  Current_Passengers =< MC,
+  Previous_Passengers is Current_Passengers-1,
+  append(Current_Ships, [[Row, Col]], Previous_Ships).
+reverse_action(right, Row, Current_Col, Passengers, Ships, Row, Previous_Col, Passengers, Ships) :- 
+  Previous_Col is Current_Col-1,
+  Previous_Col >= 0.
+reverse_action(left, Row, Current_Col, Passengers, Ships, Row, Previous_Col, Passengers, Ships) :- 
+  Previous_Col is Current_Col+1,
+  grid(_, M),
+  Previous_Col < M.
+reverse_action(up, Current_Row, Col, Passengers, Ships, Previous_Row, Col, Passengers, Ships) :- 
+  Previous_Row is Current_Row+1,
+  grid(N, _),
+  Previous_Row < N.
+reverse_action(down, Current_Row, Col, Passengers, Ships, Previous_Row, Col, Passengers, Ships) :- 
+  Previous_Row is Current_Row-1,
+  Previous_Row >= 0.
 
-% Helpers
-ship_at_location(Row, Col, Ships) :-
-  member([Row, Col], Ships).
-
-remove_ship_at_location(Row, Col, [[Row, Col]], []).
-remove_ship_at_location(Row, Col, [[Row, Col], T], [T]).
-remove_ship_at_location(Row, Col, [H, [Row, Col]], [H]).
   
 
